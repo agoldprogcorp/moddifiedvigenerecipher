89ce490b37437abcda17c579887b4c464b216134
sasha <agoldprogcorp@gmail.com>
2026-03-15 11:59:48 +0300

    fix

--- modvigenerecipher.cpp
+++ modvigenerecipher.cpp
@@ -11,9 +11,9 @@ String vigenereCipher(String txt, String key, bool encode) {
     int keyidx = 0;
     for (int i = 0; i < txt.length(); i++) {
         int idx = alphabetandnumbers.indexOf(txt.substring(i, i+1));
-        bool isDigit = isAlpha(txt.substring(i, i+1));
+        bool isDigitcheck = isDigit(char(txt.substring(i, i+1)));
         if (encode){
-            if (!isDigit){
+            if (!isDigitcheck){
                 int idx = alphabet.indexOf(txt.substring(i, i+1));
                 idx += alphabet.indexOf(key.substring(keyidx % key.length(), keyidx % key.length() + 1));
                 txtCode += alphabet.substring(idx % alphabet.length(), idx % alphabet.length() + 1);
@@ -25,16 +25,16 @@ String vigenereCipher(String txt, String key, bool encode) {
                 keyidx++;
             }
         } else {
-            if (!isDigit){
+            if (!isDigitcheck){
                 int idx = alphabet.indexOf(txt.substring(i, i+1));
                 idx -= alphabet.indexOf(key.substring(keyidx % key.length(), keyidx % key.length() + 1));
-                if (idx < 0) idx = alphabet.length() + idx;
+                if (idx < 0) { idx = alphabet.length() + idx; }
                 txtCode += alphabet.substring(idx % alphabet.length(), idx % alphabet.length() + 1);
                 keyidx++;
             } else {
                 int idx = numbers.indexOf(txt.substring(i, i+1));
                 idx -= numbers.indexOf(key.substring(keyidx % key.length(), keyidx % key.length() + 1));
-                if (idx < 0) idx numbers.length() + idx;
+                if (idx < 0) { idx numbers.length() + idx; }
                 txtCode += alphabet.substring(idx % numbers.length(), idx % alphabet.length() + 1);
                 keyidx++;
             }

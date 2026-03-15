4aa545e6383b5cfa1ae9f84124f5e8aabceeadb2
sasha <agoldprogcorp@gmail.com>
2026-03-15 12:25:53 +0300

    updated method

--- modvigenerecipher.cpp
+++ modvigenerecipher.cpp
@@ -3,42 +3,37 @@
 String vigenereCipher(String txt, String key, bool encode) {
     key.toLowerCase();
     txt.toLowerCase();
-    String alphabetandnumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
     String alphabet = "abcdefghijklmnopqrstuvwxyz";
     String numbers = "0123456789";
     String txtCode = "";
-    int idx = 0;
     int keyidx = 0;
     for (int i = 0; i < txt.length(); i++) {
-        int idx = alphabetandnumbers.indexOf(txt.substring(i, i+1));
-        char ch = txt.substring(i, i+1);
-        bool isDigit = isdigit(ch);
-        if (encode){
-            if (isDigit == false){
-                int idx = alphabet.indexOf(txt.substring(i, i+1));
-                idx += alphabet.indexOf(key.substring(keyidx % key.length(), keyidx % key.length() + 1));
-                txtCode += alphabet.substring(idx % alphabet.length(), idx % alphabet.length() + 1);
-                keyidx++;
+        char ch = txt[i];
+        int idx;
+        int shift;
+        if (alphabet.indexOf(ch) != -1) {
+            idx = alphabet.indexOf(ch);
+            shift = alphabet.indexOf(key[keyidx % key.length()]);
+            if (encode) {
+                idx = (idx + shift) % alphabet.length();
             } else {
-                int idx = numbers.indexOf(txt.substring(i, i+1));
-                idx += numbers.indexOf(key.substring(keyidx % key.length(), keyidx % key.length() + 1));
-                txtCode += numbers.substring(idx % numbers.length(), idx % numbers.length() + 1);
-                keyidx++;
+                idx = (idx - shift + alphabet.length()) % alphabet.length();
             }
+            txtCode += alphabet[idx];
+            keyidx++;
+        } 
+        else if (numbers.indexOf(ch) != -1) {
+            idx = numbers.indexOf(ch);
+            shift = alphabet.indexOf(key[keyidx % key.length()]);  
+            if (encode) {
+                idx = (idx + shift) % numbers.length();
+            } else {
+                idx = (idx - (shift % numbers.length()) + numbers.length()) % numbers.length();
+            }
+            txtCode += numbers[idx];
+            keyidx++;
         } else {
-            if (isDigit == false){
-                int idx = alphabet.indexOf(txt.substring(i, i+1));
-                idx -= alphabet.indexOf(key.substring(keyidx % key.length(), keyidx % key.length() + 1));
-                if (idx < 0) idx = alphabet.length() + idx;
-                txtCode += alphabet.substring(idx % alphabet.length(), idx % alphabet.length() + 1);
-                keyidx++;
-            } else {
-                int idx = numbers.indexOf(txt.substring(i, i+1));
-                idx -= numbers.indexOf(key.substring(keyidx % key.length(), keyidx % key.length() + 1));
-                if (idx < 0) idx = numbers.length() + idx;
-                txtCode += numbers.substring(idx % numbers.length(), idx % numbers.length() + 1);
-                keyidx++;
-            }
+            txtCode += ch;
         }
     }
     return txtCode;
